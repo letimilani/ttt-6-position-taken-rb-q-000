cbc9121142191fbb7132409b64f5e703817349c5
Leticia Comin <leticia.m.comin@gmail.com>
2016-06-24 00:44:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
 # code your #position_taken? method here!
+
+#def position_taken?(board, index)
+#  if board[index] == " " ||
+#    false
+#  elsif board[index] == ""
+#    false
+#  elsif board[index] == "X"
+#    true
+#  elsif board[index] == "O"
+#    true
+#  else
+#    false
+#  end
+#end
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
